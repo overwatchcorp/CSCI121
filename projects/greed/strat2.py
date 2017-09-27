9106b4609a31d42aa4061d687ca9927346761b6c
Jasper Fung <contact@jasperfung.co>
2017-09-26 21:59:32 -0700

    simplified strat 2, removed higher order func

--- projects/greed/strat2.py
+++ projects/greed/strat2.py
@@ -1,12 +1,17 @@
 dieMean = 2.5
 dieVariance = 2.9166666666667
 
-def create():
-    def strategy(myScore, theirScore, isLast):
-        remainingScore = 100 - myScore
-        if theirScore > myScore and myScore > 95:
-            return 1
-        if myScore < 70:
-            return int(remainingScore / dieMean * 0.86)
-        return int(remainingScore / dieMean * 0.80) 
-    return strategy
+'''
+strategy 2
+----------
+if my score is less than 70, divide the remaining score (100 - myScore) by the mean of a die roll and multiply by a multiplier (chosen mildly randomly--I just fiddled around with the numbers for a while
+if my score is more than 70, do the same thing but use a slightly lower multiplyer
+if my score is less than my opponent's score AND my score is greater than 95, roll 1 die
+'''
+def strategy(myScore, theirScore, isLast):
+    remainingScore = 100 - myScore
+    if theirScore > myScore and myScore > 95:
+        return 1
+    if myScore < 70:
+        return int(remainingScore / dieMean * 0.86)
+    return int(remainingScore / dieMean * 0.80) 
