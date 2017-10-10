f31e75fd7bb802d129e3bd3cdc7528ef17aa1251
Jasper Fung <contact@jasperfung.co>
2017-10-09 20:29:26 -0700

    working subsitution cipher

--- projects/ciphers/ciphers.py
+++ projects/ciphers/ciphers.py
@@ -1,5 +1,6 @@
 alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 import random
+import math
 random.seed()
 
 #############################################################
@@ -124,63 +125,197 @@ def caesar_break(cipher, frequencies):
 #############################################################
 
 def vigenere_enc(plain, key):
-    "your code here"
+    simplePlain = simplify_string(plain)
+    simpleKey = simplify_string(key)
+    usingKeyChar = 0
+    encoded = ''
+    for c in simplePlain:
+        if usingKeyChar >= len(simpleKey):
+            usingKeyChar = 0
+        keyChar = simpleKey[usingKeyChar]
+        shiftedChar = shift_char(c, keyChar)
+        encoded += shiftedChar
+        usingKeyChar += 1
+    return encoded
 
 def vigenere_dec(cipher, key):
-    "your code here"
-
+    simpleCipher = simplify_string(cipher)
+    simpleKey = simplify_string(key)
+    usingKeyChar = 0
+    decoded = ''
+    for c in simpleCipher:
+        if usingKeyChar >= len(simpleKey):
+            usingKeyChar = 0
+        keyChar = simpleKey[usingKeyChar]
+        keyIndex = let_to_num(keyChar)
+        revKeyIndex = keyIndex * -1
+        revKeyChar = num_to_let(revKeyIndex)
+        shiftedChar = shift_char(c, revKeyChar)
+        decoded += shiftedChar
+        usingKeyChar += 1
+    return decoded
 
 #############################################################
 # Breaking the Vigenere cipher
 #############################################################
 
 def split_string(s, parts):
-    "your code here"
-
+    currentPart = 0
+    splitStrings = []
+    for c in s:
+        # keep int currentParts less than parts minus one to stay inside array bounds
+        if currentPart > parts - 1:
+            currentPart = 0
+        # if the array already has been initialized with as many indexes as part, append to string
+        if len(splitStrings) >= parts:
+            splitStrings[currentPart] += c
+        # otherwise, add a new index with a single character as the string
+        else:
+            splitStrings.append(c)
+        currentPart += 1
+    return splitStrings
+        
 def vig_break_for_length(cipher, klen, frequencies):
-    "your code here"
+   splitStrings = split_string(cipher, klen)
+   key = ''
+   for s in splitStrings:
+       thisCaesarDec = caesar_break(s, english_freqs)
+       # we can simply append to the key string w/out specifying order because python will run the loop one after another
+       key += thisCaesarDec[0]
+   return key
 
 def vig_break(c, maxlen, frequencies):
-    "your code here"
-
-
+    decAttempts = []
+    # find best key for each possible key length
+    for x in range(1, maxlen + 1):
+        thisAttempt = vig_break_for_length(c, x, frequencies)
+        decAttempts.append(thisAttempt)
+    # decode ciphertext with each found key
+    plainAttempts = []
+    for key in decAttempts:
+        thisPlain = vigenere_dec(c, key)
+        plainAttempts.append({ 'key': key, 'plain': thisPlain })
+    # find key that produces the least distance from 
+    bestDistance = float('inf')
+    bestKey = None
+    for attempt in plainAttempts:
+        # find and normalize letter counts
+        decodeAttemptCounts = letter_counts(attempt['plain'])
+        normDecodeAttempt = normalize(decodeAttemptCounts)
+        # calculate distance from normal letter frequency
+        attemptDistance = distance(normDecodeAttempt, frequencies)
+        # if this is the smallest distance yet, set best vars equal to key and distance
+        # if two keys have the same distance, the longer one will be compared second and will not be less than
+        # the first key, so vig_break will return the shorter key
+        if attemptDistance < bestDistance:
+            bestDistance = attemptDistance
+            bestKey = attempt['key']
+    # decode the text according to best key and return
+    decoded = vigenere_dec(c, bestKey)
+    return [bestKey, decoded]
 
 #############################################################
 # A working substitution cipher
 #############################################################
 
 def sub_gen_key():
-    "your code here"
+    # # generates an array of all 26 letters, arranged randomly
+    key = list(alpha)
+    random.shuffle(key)
+    return key
 
 def sub_enc(s, k):
-    "your code here"
+    encoded = ''
+    for c in s:
+        # find position if char in alphabet
+        charIndex = let_to_num(c)
+        # find char position on scrambled alphabet
+        encChar =  k[charIndex]
+        # replace character with char from scrambled alphabet and append to encoded string
+        encoded += encChar
+    return encoded
+
 
 def sub_dec(s, k):
-    "your code here"
-
+    decoded = ''
+    for c in s:
+        # find position of char in key
+        charIndex = k.index(c)
+        # find letter that corresponds to index in actual alphabet
+        decChar = alpha[charIndex]
+        # append decoded character to decoded string
+        decoded += decChar
+    return decoded
 
 #############################################################
 # Breaking the substitution cipher
 #############################################################
 
 def count_trigrams(s):
-    "your code here"
+    trigrams = {}
+    for i, c in enumerate(s):
+        trigram = s[i:i+3]
+        if len(trigram) == 3:
+            if trigram in trigrams:
+                trigrams[trigram] += 1
+            else:
+                trigrams[trigram] = 1
+    return trigrams
+
+hitch = file_to_string('hitchhikers.txt')
+simpleHitch = simplify_string(hitch)
 
 # Uncomment the code below once the functions above are complete
-# english_trigrams = count_trigrams(simplify_string(file_to_string("twocities_full.txt")))
-# normalize(english_trigrams)
+english_trigrams = count_trigrams(simplify_string(file_to_string("twocities_full.txt")))
+normalize(english_trigrams)
 
 def map_log(d):
-    "your code here"
+    lnd = {}
+    for t in d:
+        lnd[t] = math.log(d[t])
+    return lnd
 
 # Uncomment the code below once the functions above are complete
-# map_log(english_trigrams) 
+map_log(english_trigrams) 
 
 def trigram_score(s, english_trigrams):
-    "your code here"
+    score = 0
+    sTri = count_trigrams(s)
+    lnSTri = map_log(sTri)
+    for t in lnSTri:
+        if t not in english_trigrams:
+            score -= 15
+        else:
+            score += math.log(english_trigrams[t])
+    return score
+
+def swapTwo(k):
+    newK = k
+    first = random.randint(0, 25)
+    second = random.randint(0, 25)
+    newK[first], newK[second] = newK[second], newK[first]
+    return newK
 
 def sub_break(cipher, english_trigrams):
-    "your code here"
+    randKey = sub_gen_key()
+    bestKey = randKey
+    decAttempt = sub_dec(cipher, randKey)
+    decScore = trigram_score(decAttempt, english_trigrams)
+    bestScore = decScore
+    nSinceImprovement = 0
+    while nSinceImprovement <= 1000:
+        tryKey = swapTwo(randKey[:])
+        decAttempt = sub_dec(cipher, tryKey)
+        decScore = trigram_score(decAttempt, english_trigrams)
+        if decScore > bestScore:
+            bestScore = decScore
+            randKey = tryKey
+            nSinceImprovement = 0
+        else:
+            nSinceImprovement += 1
+    decrypted = sub_dec(cipher, randKey)
+    return (bestKey, decrypted)
 
-
+mystery = file_to_string('mystery3.txt')
+print(sub_break(mystery, english_trigrams))
 
